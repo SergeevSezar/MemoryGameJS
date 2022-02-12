70b4bd70d47734bb02f504237e0cdda9753018e6
sergeev.sergey <sergeev.sezar@yandex.ru>
2022-02-12 12:30:51 +0900

    added logic created random card

--- Game/assets/scripts/GameScene.js
+++ Game/assets/scripts/GameScene.js
@@ -6,6 +6,11 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('bg', 'assets/sprites/background.png');
         this.load.image('card', 'assets/sprites/card.png');
+        this.load.image('card1', 'assets/sprites/card1.png');
+        this.load.image('card2', 'assets/sprites/card2.png');
+        this.load.image('card3', 'assets/sprites/card3.png');
+        this.load.image('card4', 'assets/sprites/card4.png');
+        this.load.image('card5', 'assets/sprites/card5.png');
     }
 
     create() {
@@ -20,9 +25,12 @@ class GameScene extends Phaser.Scene {
     createCards() {
         this.cards = [];
         let positions = this.getCardPosition();
+        Phaser.Utils.Array.Shuffle(positions);
 
-        for (let position of positions) {
-            this.cards.push(new Card(this, position));
+        for (let value of config.cards) {
+            for (let i = 0; i < 2; i++) {
+                this.cards.push(new Card(this, value, positions.pop()));
+            }
         }
     }
 
