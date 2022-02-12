19257191c58d2ce7b9461c7b9f6372d2833d3aa5
sergeev.sergey <sergeev.sezar@yandex.ru>
2022-02-12 12:13:43 +0900

    add class Cards

--- Game/assets/scripts/GameScene.js
+++ Game/assets/scripts/GameScene.js
@@ -9,12 +9,20 @@ class GameScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.sprite(0, 0, 'bg').setOrigin(0, 0);
+        this.createBackGround();
+        this.createCards();
+    }
 
+    createBackGround() {
+        this.add.sprite(0, 0, 'bg').setOrigin(0, 0);
+    }
+
+    createCards() {
+        this.cards = [];
         let positions = this.getCardPosition();
 
         for (let position of positions) {
-            this.add.sprite(position.x, position.y, 'card').setOrigin(0, 0);
+            this.cards.push(new Card(this, position));
         }
     }
 
@@ -28,7 +36,7 @@ class GameScene extends Phaser.Scene {
         let offsetY = (this.sys.game.config.height - cardHeight * config.rows) / 2;
 
         for (let row = 0; row < config.rows; row++) {
-            for (col = 0; col < config.cols; col++) {
+            for (let col = 0; col < config.cols; col++) {
                 positions.push({
                     x: offsetX + col * cardWidth,
                     y: offsetY + row * cardHeight,
